ab88f01e5bd53e41830260b8baa9fa719ad5a4a4
Minitmar <tmaroberg@gmail.com>
2025-03-16 13:53:03 +0100

    Setup eslint and prettier to improve code style and quality.

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat['jsx-runtime'],
+    settings: {
+      react: {
+        pragma: 'React',
+        fragment: 'Fragment',
+        version: '19.0.0',
+        defaultVersion: '19.0.0',
+      },
+    },
+  },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: { jsx: true },
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
@@ -25,4 +48,5 @@ export default tseslint.config(
       ],
     },
   },
-)
+  eslintConfigPrettier
+);
